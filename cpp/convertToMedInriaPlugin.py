e57b74b5fa48651437ddf5240e56a2e4cf87cb9d
Arthur Masson <arthur.masson@inria.fr>
2019-08-19 10:28:51 +0200

    v1 completed

--- cpp/convertToMedInriaPlugin.py
+++ cpp/convertToMedInriaPlugin.py
@@ -2,9 +2,17 @@ import os
 import re
 import string
 
+# This script converts BoutiqueGUI standalone code to a medInria plugin
+# It duplicates and rename the BoutiquesGUI .h and .cpp files, and rename classes to follow medInria naming standards
+# The output files go to the medInriaPlugin directory
+# Those files can then be copied to medInria-sources/src/plugins/legacy/medBoutiques/boutiquesGUI/ and the medInria plugin should compile
+
+
+# Prefix of the plugin
 namePrefix = "medBoutiques"
 namePrefixCapital = namePrefix.upper()
 
+# Standalone file names to medInria plugin file names (the new names will be prefixed with the namePrefix)
 nameMap = {
 	"executionwidget": "ExecutionWidget",
 	"invocationwidget": "InvocationWidget",
@@ -13,8 +21,10 @@ nameMap = {
 	"dropwidget": "DropWidget",
 	"abstractfilehandler": "AbstractFileHandler",
 	"filehandler": "FileHandler",
+	"configuration": "Configuration"
 }
 
+# List the src/ directory and duplicates each file in nameMap, rename it and output it to medInriaPlugin/
 for root, dirs, files in os.walk("src/", topdown=False):
 	for name in files:
 		(basename, extension) = os.path.splitext(name)
@@ -31,6 +41,7 @@ for root, dirs, files in os.walk("src/", topdown=False):
 				with open(os.path.join(root, name), "r") as inputFile:
 					for line in inputFile:
 
+						# Rename include guards
 						isDefine = False
 						for linePrefix in ["#ifndef ", "#define ", "#endif // "]:
 							if line.startswith(linePrefix + baseNameCapital + "_H"):
@@ -39,6 +50,7 @@ for root, dirs, files in os.walk("src/", topdown=False):
 
 						isInclude = False
 
+						# Rename includes
 						include_search = re.search('#include "(.*).h"', line)
 						if include_search:
 							includedFileName = include_search.group(1)
@@ -46,11 +58,17 @@ for root, dirs, files in os.walk("src/", topdown=False):
 								line = line.replace(includedFileName, namePrefix + nameMap[includedFileName])
 							isInclude = True
 
+						# Comment #define BOUTIQUE_GUI_STANDALONE
+						if line.startswith("#define BOUTIQUE_GUI_STANDALONE"):
+							line = line.replace("#define BOUTIQUE_GUI_STANDALONE", "// #define BOUTIQUE_GUI_STANDALONE")
+
+						# Rename class names
 						if not isDefine and not isInclude:
 
 							for oldName, newName in nameMap.items():
 								# line = line.replace(newName, namePrefix + newName)
 								line = re.sub(r"\b%s\b" % newName , namePrefix + newName, line)
 
+						# Write file
 						outputFile.write(line)
 						# print(line, file=outputFile)
